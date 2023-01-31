81cc1a39bf796956dad60a2484f022497f0c46fb
goutam2018 <goutam.sarkar94@gmail.com>
2023-01-31 23:33:17 +0530

    httpd script added

--- monit.sh
+++ monit.sh
@@ -1,15 +1,26 @@
 #/bin/bash
 
 echo "#####################################"
+date
+#ls /var/run/httpd/httpd.pid &> /dev/null
 
-ls /var/run/httpd/httpd.pid &> /dev/null
-
-if [ $? -eq 0 ]
+if [ -f  /var/run/httpd/httpd.pid ]
 then 
   echo "Httpd Service is running."
 else
   echo "Httpd Service is not running."
-  echo starting process
+  echo "starting httpd process"
+  
+  systemctl start httpd
+  if [ $? -eq 0 ]
+  then 
+    echo "Process started successfuly."
+  else
+    echo "Process starting failed, contact system admin"
+  fi
+fi
+echo "######################################"
+echo
 
 
 
